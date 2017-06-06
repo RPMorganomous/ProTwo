a1f77b51a7ee67732a04dcbd34452b9e5ecde38b
Richard Morgan <mba.rick@gmail.com>
2017-06-05 19:19:34 -0500

    ProTwo template working

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<em>My Second App</em>")
+    # return HttpResponse("<em>My Second App</em>")
+    my_dict = {'insert_me':"Help Page"}
+    return render(request, 'AppTwo/index.html', context=my_dict)
